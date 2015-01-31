a902c6087db7b451fefadd05c2a40705d7b4df01
Mike Herold <archangel.herold@gmail.com>
2015-01-31 00:38:11 -0800

    User support for IRC.
    The bot will read the users shown at join, and keep a list of users and mod status in memory. joins and parts will also impact the user list.
    
    Refactored the IRC class quite a bit.

--- src/bot.py
+++ src/bot.py
@@ -10,6 +10,7 @@ import lib.irc as irc_
 from lib.functions_general import *
 from lib.command import Command
 from lib.messages import Messages
+from lib.users import Users
 import sys
 import time
 import logging
@@ -20,16 +21,17 @@ class RoboRaj():
         self.config = config
         self.irc = irc_.IRC(config)
         self.socket = self.irc.get_irc_socket_object()
-        self.command_class = Command(self.config)
-        self.commands_dict = self.command_class.get_commands()
-        self.messages_class = Messages(save_type="sqlite")
+        self.Command = Command(self.config)
+        self.commands_dict = self.Command.get_commands()
+        self.Messages = Messages(save_type="sqlite")
+        self.Users = Users()
         logging.basicConfig(filename=log_filename,
                             level=logging.DEBUG,
                             format='%(asctime)s %(message)s',
                             datefmt='%m/%d/%Y %I:%M:%S %p')
 
     def run(self):
-        irc = self.irc
+        irc_stream = self.irc
         sock = self.socket
         config = self.config
 
@@ -40,66 +42,82 @@ class RoboRaj():
                 logging.error('Connection was lost, reconnecting.')
                 sock = self.irc.get_irc_socket_object()
 
-            # check for ping, reply with pong
-            irc.check_for_ping(data)
+            for data_event in data.splitlines():
+                event_data = irc_stream.process_data(data_event)
 
-            if irc.check_for_message(data):
-                message_dict = irc.get_message(data)
+                if irc_stream.is_join_message(event_data):
+                    self.Users.add_to_channel(username=event_data['info']['username'],
+                                              channel=event_data['info']['channel'])
+                elif irc_stream.is_part(event_data):
+                    self.Users.remove_from_channel(username=event_data['info']['username'],
+                                                   channel=event_data['info']['channel'])
+                elif irc_stream.is_mode_message(event_data):
+                    channel = event_data['info']['channel']
+                    username = event_data['info']['username']
+                    if event_data['info']['status'] == 'op':
+                        self.Users.mod_user(channel=channel, username=username)
+                    if event_data['info']['status'] == 'deop':
+                        self.Users.unmod_user(channel=channel, username=username)
+                elif irc_stream.is_list_message(event_data):
+                    for user_info in event_data['info']['users']:
+                        self.Users.add_to_channel(username=user_info["username"], channel=event_data['info']['channel'])
+                elif irc_stream.is_priv_message(event_data):
+                    timestamp = time.time()
+                    channel = event_data['info']['channel']
+                    message = event_data['info']['message']
+                    username = event_data['info']['username']
 
-                timestamp = time.time()
-                channel = message_dict['channel']
-                message = message_dict['message']
-                username = message_dict['username']
-
-                if config['debug']:
-                    logging.debug("channel: %s | username: %s | message: %s" % (channel, username, message))
-
-                self.messages_class.store_message(channel, username, message, timestamp)
-
-                # check if message is a command with no arguments
-                if self.command_class.is_valid_command(message) or self.command_class.is_valid_command(message.split(' ')[0]):
-                    command = message
-                    only_command = command.split(' ')[0]
-                    command_user_level = self.command_class.get_command_user_level(only_command)
+                    self.Users.update(username, channel)
 
                     if config['debug']:
-                        logging.debug("command_class: %s", command_user_level)
+                        logging.debug("channel: %s | username: %s | message: %s" % (channel, username, message))
 
-                    if self.command_class.is_authorized(command_user_level, username):
-                        if self.command_class.check_returns_function(command.split(' ')[0]):
-                            if self.command_class.check_has_correct_args(command, command.split(' ')[0]):
-                                args = command.split(' ')
-                                del args[0]
+                    self.Messages.store_message(channel, username, message, timestamp)
 
-                                command = command.split(' ')[0]
-                                if self.command_class.is_on_cooldown(command, channel):
-                                    cooldown_remaining = self.command_class.get_cooldown_remaining(command, channel)
+                    # check if message is a command with no arguments
+                    if self.Command.is_valid_command(message) or self.Command.is_valid_command(message.split(' ')[0]):
+                        command = message
+                        only_command = command.split(' ')[0]
+                        command_user_level = self.Command.get_command_user_level(only_command)
+
+                        if config['debug']:
+                            logging.debug("command_class: %s", command_user_level)
+
+                        if self.Command.is_authorized(command_user_level, username):
+                            if self.Command.check_returns_function(command.split(' ')[0]):
+                                if self.Command.check_has_correct_args(command, command.split(' ')[0]):
+                                    args = command.split(' ')
+                                    del args[0]
+
+                                    command = command.split(' ')[0]
+                                    if self.Command.is_on_cooldown(command, channel):
+                                        cooldown_remaining = self.Command.get_cooldown_remaining(command, channel)
+                                        logging.info('Command (%s) is on cooldown for user (%s) (%ss remaining)'
+                                                     % (command, username, cooldown_remaining))
+                                    else:
+                                        logging.info('Command (%s) received by user (%s)'
+                                                     % (command, username))
+                                        result = self.Command.pass_to_function(command, args, channel)
+                                        self.Command.update_last_used(command, channel)
+
+                                        if result:
+                                            resp = '(%s) > %s' % (username, result)
+                                            print_bot_message(resp, channel)
+                                            irc_stream.send_message(channel, resp)
+                            else:
+                                if self.Command.is_on_cooldown(command, channel):
                                     logging.info('Command (%s) is on cooldown for user (%s) (%ss remaining)'
-                                                 % (command, username, cooldown_remaining))
-                                else:
-                                    logging.info('Command (%s) received by user (%s)'
-                                                 % (command, username))
-                                    result = self.command_class.pass_to_function(command, args, channel)
-                                    self.command_class.update_last_used(command, channel)
+                                                 % (command, username, self.Command.get_cooldown_remaining(command, channel)))
+                                elif self.Command.check_has_return(command):
+                                    logging.info('Command (%s) received by user (%s) on channel (%s)'
+                                                 % (command, username, channel))
+                                    self.Command.update_last_used(command, channel)
 
-                                    if result:
-                                        resp = '(%s) > %s' % (username, result)
-                                        print_bot_message(resp, channel)
-                                        irc.send_message(channel, resp)
+                                    resp = '(%s) > %s' % (username, self.Command.get_return(command))
+                                    self.Command.update_last_used(command, channel)
+
+                                    print_bot_message(resp, channel)
+                                    irc_stream.send_message(channel, resp)
                         else:
-                            if self.command_class.is_on_cooldown(command, channel):
-                                logging.info('Command (%s) is on cooldown for user (%s) (%ss remaining)'
-                                             % (command, username, self.command_class.get_cooldown_remaining(command, channel)))
-                            elif self.command_class.check_has_return(command):
-                                logging.info('Command (%s) received by user (%s) on channel (%s)'
-                                             % (command, username, channel))
-                                self.command_class.update_last_used(command, channel)
-
-                                resp = '(%s) > %s' % (username, self.command_class.get_return(command))
-                                self.command_class.update_last_used(command, channel)
-
-                                print_bot_message(resp, channel)
-                                irc.send_message(channel, resp)
-                    else:
-                        irc.send_message(channel, "User not authorized")
-                        logging.info("(%s) Not authorized to run command." % (username,))
+                            irc_stream.send_message(channel, "User not authorized")
+                            logging.info("(%s) Not authorized to run command." % (username,))
--- src/lib/irc.py
+++ src/lib/irc.py
@@ -10,35 +10,156 @@ ping_no_response_threshold = 5 * 60  # five minutes, make this whatever you want
 class IRC:
     def __init__(self, config):
         self.config = config
+        self.sock = None
 
-    def check_for_message(self, data):
-        if re.match(r'^:[a-zA-Z0-9_]+\![a-zA-Z0-9_]+@[a-zA-Z0-9_]+(\.tmi\.twitch\.tv|\.testserver\.local) PRIVMSG #[a-zA-Z0-9_]+ :.+$', data):
-            return True
+    def process_data(self, data):
+        data_split = data.split(" ")
 
-    def check_is_command(self, message, valid_commands):
-        for command in valid_commands:
-            if command == message:
-                return True
+        # When the IRC client connects to a channel.
+        if "PRIVMSG" in data_split[1]:
+            event_information = {
+                'event': 'message',
+                'info': self.parse_message(data)
+            }
+            return event_information
+        elif "PING" in data_split[0]:
+            event_information = {
+                'event': 'ping'
+            }
+            return event_information
+        elif "PART" in data_split[1]:
+            event_information = {
+                'event': 'part',
+                'info': self.parse_part(data)
+            }
+            return event_information
+        elif "JOIN" in data_split[1]:
+            event_information = {
+                'event': 'join',
+                'info': self.parse_join(data)
+            }
+            return event_information
+        elif "MODE" in data_split[1]:
+            event_information = {
+                'event': 'mode',
+                'info': self.parse_mode(data)
+            }
+            return event_information
+        elif "353" in data_split[1]:
+            event_information = {
+                'event': 'list',
+                'info': self.parse_channel_user_list(data)
+            }
+            return event_information
+        else:
+            return {'event': 'unknown'}
 
-    def check_for_connected(self, data):
-        if re.match(r'^:.+ 001 .+ :connected to TMI$', data):
-            return True
+    def parse_channel_user_list(self, data):
+        data_split = data.split("=")[1].lstrip().split(" ")
+        channel = data_split[0]
 
-    def check_for_ping(self, data):
+        event_info = {'channel': channel, 'users': []}
+
+        for user in data_split[1:]:
+            user = user.lstrip(":")
+            event_info['users'].append({'username': user})
+        return event_info
+
+    def parse_mode(self, data):
+        status = ''
+        if '+o' in data.split(" ")[3]:
+            status = "op"
+        elif '-o' in data.split(" ")[3]:
+            status = "deop"
+
+        channel = data.split(" ")[2]
+        username = data.split(" ")[4]
+        return {
+            'status': status,
+            'channel': channel,
+            'username': username
+        }
+
+    def parse_part(self, data):
+        nickname = data.split("!")[0].lstrip(":").rstrip('\r')
+        username = data.split("!")[1].split("@")[0].rstrip('\r')
+        channel = re.findall(r'PART #(.+)', data)[0]
+        return {
+            'nickname': nickname,
+            'username': username,
+            'channel': channel
+        }
+
+    def parse_join(self, data):
+        nickname = data.split("!")[0].lstrip(":").rstrip('\r')
+        username = data.split("!")[1].split("@")[0].rstrip('\r')
+        channel = re.findall(r'JOIN (.+)', data)[0]
+        return {
+            'nickname': nickname,
+            'username': username,
+            'channel': channel
+        }
+
+    def parse_ping(self, data):
         last_ping = time.time()
         #if data[0:4] == "PING":
         if data.find('PING') != -1:
-            self.sock.send('PONG ' + data.split() [ 1 ] + '\r\n')
+            self.sock.send('PONG ' + data.split()[1] + '\r\n')
             last_ping = time.time()
         if (time.time() - last_ping) > ping_no_response_threshold:
             sys.exit()
 
-    def get_message(self, data):
+    def parse_message(self, data):
         return {
-        'channel': re.findall(r'^:.+\![a-zA-Z0-9_]+@[a-zA-Z0-9_]+.+ PRIVMSG (.*?) :', data)[0],
-        'username': re.findall(r'^:([a-zA-Z0-9_]+)\!', data)[0],
-        'message': re.findall(r'PRIVMSG #[a-zA-Z0-9_]+ :(.+)', data)[0].decode('utf8')
+            'channel': re.findall(r'^:.+\![a-zA-Z0-9_]+@[a-zA-Z0-9_]+.+ PRIVMSG (.*?) :', data)[0],
+            'username': re.findall(r'^:([a-zA-Z0-9_]+)\!', data)[0],
+            'message': re.findall(r'PRIVMSG #[a-zA-Z0-9_]+ :(.+)', data)[0].decode('utf8')
         }
+
+    def is_priv_message(self, event):
+        try:
+            if event['event'] == 'message':
+                return True
+            else:
+                return False
+        except KeyError:
+            return False
+
+    def is_list_message(self, event):
+        try:
+            if event['event'] == 'list':
+                return True
+            else:
+                return False
+        except KeyError:
+            return False
+
+    def is_join_message(self, event):
+        try:
+            if event['event'] == 'join':
+                return True
+            else:
+                return False
+        except KeyError:
+            return False
+
+    def is_part(self, event):
+        try:
+            if event['event'] == 'part':
+                return True
+            else:
+                return False
+        except KeyError:
+            return False
+
+    def is_mode_message(self, event):
+        try:
+            if event['event'] == 'mode':
+                return True
+            else:
+                return False
+        except KeyError:
+            return False
 
     def check_login_status(self, data):
         if re.match(r'^:(testserver\.local|tmi\.twitch\.tv) NOTICE \* :Login unsuccessful\r\n$', data):
@@ -58,7 +179,8 @@ class IRC:
         try:
             sock.connect((self.config['server'], self.config['port']))
         except:
-            print_bot_status_message('Cannot connect to server (%s:%s).' % (self.config['server'], self.config['port']), 'error')
+            print_bot_status_message('Cannot connect to server (%s:%s).' %
+                                     (self.config['server'], self.config['port']), 'error')
             sys.exit()
 
         sock.settimeout(None)
