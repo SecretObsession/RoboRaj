446a0217f423faea7b6ed8c88d4ab07d53ce2810
Mike Herold <archangel.herold@gmail.com>
2015-02-01 18:16:51 -0800

    Migrated addtextresponse to roboraj commands
    New command is !roboraj addcommand <command_name> <message>

--- src/lib/commands/roboraj.py
+++ src/lib/commands/roboraj.py
@@ -15,11 +15,31 @@ def roboraj(robo_info):
     args = robo_info['command_info']['args']
     channel = robo_info['command_info']['channel']
     user = robo_info['command_info']['user']
-    #commands = Command(config)
 
     if args[0] == "removecommand":
         Commands.remove_command(command=args[1])
         return "Command removed"
+
+    elif args[0] == "addcommand":
+        command = args[1]
+        # set a hardcoded limit because of having to deal with large strings and potential for quotes
+        command_cooldown = 10
+        response_message = ""
+
+        for argindex in range(2, len(args)):
+            response_message += " %s" % (args[argindex])
+
+        new_command = {
+            'limit': command_cooldown,
+            'return': response_message.lstrip()
+        }
+
+        # add the new command to the available commands list
+        new_command_key = "!"+command
+        Commands.commands[new_command_key] = new_command
+        Commands.save_command_memory()
+
+        return "Added command as an available command"
 
     elif args[0] == "dump_users":
         user_list = Users.list_users(channel=channel)
